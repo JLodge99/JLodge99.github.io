3d73dce088db7321ca7bd8a08dfb629b1e8cd235
Jerold <jrld1999@gmail.com>
2021-04-03 18:56:41 -0400

    Update

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const projectcard = document.querySelectorAll(".project_card")
 const githublinks = ["https://github.com/JLodge99/Everglades-Server", 
                     "https://github.com/JLodge99/Amigo-Mobile",
-                    "https://github.com/JLodge99/My-Contacts"]
+                    "https://github.com/JLodge99/My-Contacts",
+                    "https://www.google.com"]
 projectcard.forEach((x, index) => {
     x.addEventListener("click", () => {
         window.open(githublinks[index])
