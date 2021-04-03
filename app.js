743a9c7bbf854dbf49eebc564815b54b87a17de7
Jerold <jrld1999@gmail.com>
2021-04-03 18:31:27 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const projectcard = document.querySelectorAll(".project_card")
+const githublinks = ["https://github.com/JLodge99/Everglades-Server", 
+                    "https://github.com/JLodge99/Amigo-Mobile",
+                    "https://github.com/JLodge99/My-Contacts"]
+projectcard.forEach((x, index) => {
+    x.addEventListener("click", () => {
+        window.open(githublinks[index])
+    })
+    
+})
