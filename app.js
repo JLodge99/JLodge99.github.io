c365a534d116773505dfe2725c72b89073b7b30b
Jerold <jrld1999@gmail.com>
2021-04-14 01:27:58 -0400

    New home page

--- app.js
+++ app.js
@@ -1,117 +1,161 @@
-const projectcard = document.querySelectorAll(".project_card")
-const githublinks = ["https://github.com/JLodge99/Everglades-Server", 
-                    "https://github.com/JLodge99/Amigo-Mobile",
-                    "https://github.com/JLodge99/My-Contacts",
-                    "",
-                    ""]
-const mobile_menu = document.querySelector("#mobile_menu_btn")
-const navmenu = document.querySelector(".navbar_buttons")
-const navlinks = document.querySelectorAll(".navbar_item") 
-const scroll_links = document.querySelectorAll("#scroll_link")
-const aboutcontainer = document.querySelector(".aboutcontainer")
-var selectedNavLink = -1
+const projectcard = document.querySelectorAll(".project_card");
+const githublinks = [
+  "https://github.com/JLodge99/Everglades-Server",
+  "https://github.com/JLodge99/Amigo-Mobile",
+  "https://github.com/JLodge99/My-Contacts",
+  "",
+  "",
+];
+const mobile_menu = document.querySelector("#mobile_menu_btn");
+const mobilebar = document.querySelectorAll(".bar");
+const navbar = document.querySelector(".navbar");
+const navmenu = document.querySelector(".navbar_buttons");
+const navlinks = document.querySelectorAll(".navbar_item");
+const scroll_links = document.querySelectorAll("#scroll_link");
+const homecontainer = document.querySelector("#homecontainer");
+const aboutcontainer = document.querySelector(".aboutcontainer");
+const projectcontainer = document.querySelector(".project_main");
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+const letters = document.querySelector(".letters");
+const titlename = document.getElementById("titlename");
+const aboutmebtn = document.querySelector(".toabout");
+
+var currentIndex = -1;
+var navtoggle = false;
+var canvasInView = true;
 
 // Redirect logic for project cards
 projectcard.forEach((x, index) => {
-    x.addEventListener("click", () => {
-        window.open(githublinks[index])
-    })
-    
-})
+  x.addEventListener("click", () => {
+    window.open(githublinks[index]);
+  });
+});
 
 // Mobile menu toggle
 mobile_menu.addEventListener("click", () => {
-    navmenu.classList.toggle("active")
-})
+  mobile_menu.classList.toggle("is-active");
+  navmenu.classList.toggle("active");
+});
+
+aboutmebtn.addEventListener("click", () => {
+  const yOffset = -65;
+  const y =
+    scroll_links[0].getBoundingClientRect().top + window.pageYOffset + yOffset;
+
+  window.scrollTo({ top: y, behavior: "smooth" });
+});
 
 // Navbar link logic
 navlinks.forEach((element, key) => {
-    if (key == 0) {
-        element.addEventListener("click", () => {
-            window.scrollTo(0, 0)
-            navmenu.classList.toggle("active", false)
-        })    
-    }
-    else if(key == 1){
-        element.addEventListener("click", () => {
-            const yOffset = -50
-            const y = scroll_links[0].getBoundingClientRect().top + window.pageYOffset + yOffset
-        
-            window.scrollTo({top: y, behavior: 'smooth'})
-            navmenu.classList.toggle("active", false)
-        })
-    }
-})
+  if (key == 0) {
+    element.addEventListener("click", () => {
+      window.scrollTo(0, 0);
+      navmenu.classList.toggle("active", false);
+      mobile_menu.classList.toggle("is-active", false);
+    });
+  } else if (key == 1) {
+    element.addEventListener("click", () => {
+      const yOffset = -65;
+      const y =
+        scroll_links[0].getBoundingClientRect().top +
+        window.pageYOffset +
+        yOffset;
+
+      window.scrollTo({ top: y, behavior: "smooth" });
+      navmenu.classList.toggle("active", false);
+      mobile_menu.classList.toggle("is-active", false);
+    });
+  } else if (key == 2) {
+    element.addEventListener("click", () => {
+      const yOffset = -65;
+      const y =
+        scroll_links[1].getBoundingClientRect().top +
+        window.pageYOffset +
+        yOffset;
+
+      window.scrollTo({ top: y, behavior: "smooth" });
+      navmenu.classList.toggle("active", false);
+      mobile_menu.classList.toggle("is-active", false);
+    });
+  }
+});
 
 // SVG animation logic
-var a = document.getElementById("main_img")
-a.addEventListener("load",function(){
-
+var a = document.getElementById("main_img");
+a.addEventListener(
+  "load",
+  function () {
     // get the inner DOM of alpha.svg
     var svgDoc = a.contentDocument;
     // get the inner element by id
-    var delta = [svgDoc.getElementById("codeline1"),
-    svgDoc.getElementById("codeline2"),
-    svgDoc.getElementById("codeline3"),
-    svgDoc.getElementById("codeline4") ];
+    var delta = [
+      svgDoc.getElementById("codeline1"),
+      svgDoc.getElementById("codeline2"),
+      svgDoc.getElementById("codeline3"),
+      svgDoc.getElementById("codeline4"),
+    ];
 
     anime({
-        targets: delta,
-        loop: true,
-        direction: 'alternate',
-        scale: 1,
-        translateY: -10,
-        easing: 'easeInOutCubic',
-        delay: anime.stagger(20)
-    })
+      targets: delta,
+      loop: true,
+      direction: "alternate",
+      scale: 1,
+      translateY: -10,
+      easing: "easeInOutCubic",
+      delay: anime.stagger(20),
+    });
+  },
+  false
+);
 
-}, false)
+function getNavIndexByScrollPos(scrollPosition) {
+  let navheight = 65;
+  if (scrollPosition > projectcontainer.offsetTop - 200) return 2;
+  else if (scrollPosition > aboutcontainer.offsetTop - 200) return 1;
+  else if (scrollPosition > 0) return 0;
 
-function toggleNavTuck(index){
-
-    navlinks.forEach((element, key) => {
-        //element.style.backgroundColor = "white"
-        element.classList.remove('navbar_item_select')
-    })
-
-    if(index != -1){
-        navlinks[index].classList.add('navbar_item_select')
-        // document.querySelector(".navbar_logo").classList.add('nomargin')
-        document.querySelector(".navbar_buttons").classList.add('navbar_buttons_select')
-        selectedNavLink = index
-    }
-    else{
-        selectedNavLink = -1
-        // document.querySelector(".navbar_logo").classList.remove('nomargin')
-        document.querySelector(".navbar_buttons").classList.remove('navbar_buttons_select')
-    }
+  return -1;
 }
 
-function getNavIndexByScrollPos(scrollPosition){
-    if(scrollPosition > aboutcontainer.offsetHeight)
-        return 1
-    else if(scrollPosition > 0)
-        return 0
-    
-    return -1
-}
-
-console.log(aboutcontainer.offsetHeight)
-
-let tick = false
-document.addEventListener('scroll', () => {
-    scrollpos = window.scrollY
-    if(!tick){
-        if((index = getNavIndexByScrollPos(scrollpos)) != selectedNavLink){
-            console.log("Index: " + index)
-            console.log("Selected: " + selectedNavLink)
-            tick = true
-            window.requestAnimationFrame(() => {
-                toggleNavTuck(index)
-                tick = false;
-                console.log("Done")
-            })
+let tick = false;
+document.addEventListener("scroll", () => {
+  scrollpos = window.scrollY;
+  if (!tick) {
+    if ((index = getNavIndexByScrollPos(scrollpos)) != currentIndex) {
+      currentIndex = index;
+      console.log("Index: " + index);
+      console.log("Selected: " + currentIndex);
+      tick = true;
+      requestAnimationFrame(() => {
+        tick = false;
+        if (currentIndex != -1 && !navtoggle) {
+          navbar.classList.toggle("active", true);
+          mobilebar.forEach((element, key) => {
+            element.classList.toggle("active", true);
+          });
+          navlinks.forEach((element, key) => {
+            element.classList.toggle("active", true);
+          });
+          navtoggle = true;
+        } else if (currentIndex == -1 && navtoggle) {
+          mobilebar.forEach((element, key) => {
+            element.classList.toggle("active", false);
+          });
+          navbar.classList.toggle("active", false);
+          navlinks.forEach((element, key) => {
+            element.classList.toggle("active", false);
+          });
+          navtoggle = false;
         }
+        console.log("Done");
+      });
     }
-    console.log(index)
-})
+
+    if (canvas.getBoundingClientRect().bottom - 65 < 0) canvasInView = false;
+    else if (!canvasInView && canvas.getBoundingClientRect().bottom - 65 > 0) {
+      canvasInView = true;
+      animate();
+    }
+  }
+});
---
+++ background.js
@@ -0,0 +1,228 @@
+const particlesArray = [];
+var hue = 0;
+
+var density = 100;
+setDensity();
+
+var distance = 100;
+setDistance();
+
+class Particle {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 15 + 5;
+    this.speedX = Math.random() - 0.5;
+    this.speedY = Math.random() - 0.5;
+    this.color = hue;
+    hue += 300 / density;
+  }
+
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+
+  draw() {
+    ctx.fillStyle = "rgba(148, 148, 148, 0.5)";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, 5, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+function handleParticles() {
+  for (let i = 0; i < particlesArray.length; i++) {
+    particlesArray[i].update();
+    particlesArray[i].draw();
+
+    for (let j = 0; j < particlesArray.length; j++) {
+      const dx = particlesArray[i].x - particlesArray[j].x;
+      const dy = particlesArray[i].y - particlesArray[j].y;
+      const dist = Math.sqrt(dx * dx + dy * dy);
+      if (dist <= distance) {
+        let lightness = (dist / distance) * 100;
+        let grad = ctx.createLinearGradient(
+          particlesArray[i].x,
+          particlesArray[i].y,
+          particlesArray[j].x,
+          particlesArray[j].y
+        );
+        grad.addColorStop(
+          0,
+          "hsla(" +
+            particlesArray[i].color +
+            ", 100%, 50%, " +
+            (1 - dist / distance) +
+            ")"
+        );
+        grad.addColorStop(
+          1,
+          "hsla(" +
+            particlesArray[j].color +
+            ", 100%, 50%, " +
+            (1 - dist / distance) +
+            ")"
+        );
+        ctx.strokeStyle = grad;
+        ctx.lineWidth = 3;
+        ctx.beginPath();
+        ctx.moveTo(particlesArray[i].x, particlesArray[i].y);
+        ctx.lineTo(particlesArray[j].x, particlesArray[j].y);
+        ctx.stroke();
+      }
+    }
+
+    if (
+      particlesArray[i].x < -20 ||
+      particlesArray[i].x > canvas.width + 20 ||
+      particlesArray[i].y < -20 ||
+      particlesArray[i].y > canvas.height + 20
+    ) {
+      particlesArray.splice(i, 1);
+      i--;
+    } else if (particlesArray[i].x <= 0) particlesArray[i].speedX *= -1;
+    else if (particlesArray[i].y <= 0) particlesArray[i].speedY *= -1;
+    else if (particlesArray[i].x > canvas.width) particlesArray[i].speedX *= -1;
+    else if (particlesArray[i].y > canvas.height)
+      particlesArray[i].speedY *= -1;
+  }
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  if (!canvasInView) return;
+
+  if (particlesArray.length < density) particlesArray.push(new Particle());
+  else if (particlesArray.length > density) particlesArray.pop();
+
+  handleParticles();
+  requestAnimationFrame(animate);
+}
+
+function burst(count) {
+  for (let i = 0; i < count; i++) {
+    particlesArray.push(new Particle());
+  }
+}
+
+function introanimation() {
+  anime
+    .timeline()
+    .add({
+      targets: "#titlename",
+      opacity: 1,
+    })
+    .add({
+      targets: ".letter",
+      opacity: [0, 1],
+      easing: "easeOutExpo",
+      translateY: [200, 0],
+      duration: 500,
+      delay: (el, i) => 70 * (i + 1),
+    })
+    .add({
+      targets: ".line",
+      scaleX: [0, 1],
+      opacity: [0, 1],
+      easing: "easeOutExpo",
+      duration: 700,
+      offset: "-=875",
+      delay: (el, i, l) => 80 * (l - i),
+    })
+    .add(
+      {
+        targets: aboutmebtn,
+        opacity: [0, 1],
+        translateY: [-70, 0],
+        duration: 1500,
+      },
+      "-=200"
+    );
+}
+
+function setDensity() {
+  if (window.innerWidth <= 600) {
+    density = 50;
+  } else if (window.innerWidth <= 1024) density = 70;
+  else density = 100;
+}
+
+function setDistance() {
+  if (window.innerWidth <= 600) {
+    distance = 70;
+  } else if (window.innerWidth <= 1024) distance = 80;
+  else distance = 100;
+}
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+letters.innerHTML = letters.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+
+const test = anime
+  .timeline()
+  .add({
+    targets: "#titlename",
+    opacity: 1,
+  })
+  .add({
+    targets: ".letter",
+    opacity: [0, 1],
+    easing: "easeOutExpo",
+    translateY: [200, 0],
+    duration: 500,
+    delay: (el, i) => 70 * (i + 1),
+  })
+  .add({
+    targets: ".line",
+    scaleX: [0, 1],
+    opacity: [0, 1],
+    easing: "easeOutExpo",
+    duration: 700,
+    offset: "-=875",
+    delay: (el, i, l) => 80 * (l - i),
+  })
+  .add(
+    {
+      targets: aboutmebtn,
+      opacity: [0, 1],
+      translateY: [-70, 0],
+      duration: 1500,
+    },
+    "-=200"
+  );
+
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  setDensity();
+});
+
+/*
+titlename.addEventListener("click", (event) => {
+  
+})
+
+const titlenameX = titlename.getBoundingClientRect().left
+const titlenameY = titlename.offsetTop - titlename.offsetHeight
+const titlenameWidth = titlename.offsetWidth
+const titlenameHeight = titlename.offsetHeight
+*/
+
+animate();
+
+test.play();
+
+canvas.addEventListener("click", () => {
+  if (!canvasInView) {
+    console.log("Starting animation");
+    canvasInView = true;
+    animate();
+  } else {
+    canvasInView = false;
+  }
+});
